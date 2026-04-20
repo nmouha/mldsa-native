391c029d2edecb6b4c9077caefeab3a50af7b39e
github-action-benchmark <github@users.noreply.github.com>
2026-04-20 08:20:47 +0000

    add Graviton2 (customSmallerIsBetter) benchmark result for 7cc8dc1122ce9876608b7e16084c95fb6aae7695

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1776673214767,
+  "lastUpdate": 1776673246922,
   "repoUrl": "https://github.com/pq-code-package/mldsa-native",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks (opt)": [
@@ -486478,6 +486478,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-DSA-87 verify",
             "value": 320069,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "matthias@zerorisc.com",
+            "name": "Matthias J. Kannwischer",
+            "username": "mkannwischer"
+          },
+          "committer": {
+            "email": "matthias@kannwischer.eu",
+            "name": "Matthias J. Kannwischer",
+            "username": "mkannwischer"
+          },
+          "distinct": true,
+          "id": "7cc8dc1122ce9876608b7e16084c95fb6aae7695",
+          "message": "Armv8.1-M: Add CFI directives for stack unwinding\n\nPorts https://github.com/pq-code-package/mlkem-native/pull/1558\n\nSigned-off-by: Matthias J. Kannwischer <matthias@zerorisc.com>",
+          "timestamp": "2026-04-20T10:14:23+02:00",
+          "tree_id": "4cd053e62d2362a776ad84c446a3710a451b888e",
+          "url": "https://github.com/pq-code-package/mldsa-native/commit/7cc8dc1122ce9876608b7e16084c95fb6aae7695"
+        },
+        "date": 1776673052902,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-DSA-44 keypair",
+            "value": 112603,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 sign",
+            "value": 356703,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 verify",
+            "value": 118040,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 keypair",
+            "value": 195024,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 sign",
+            "value": 587526,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 verify",
+            "value": 194321,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 keypair",
+            "value": 320970,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 sign",
+            "value": 753974,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 verify",
+            "value": 319985,
             "unit": "cycles"
           }
         ]
