cccda2d16fde6f4651ac6027d8b6e58a8297af34
github-action-benchmark <github@users.noreply.github.com>
2026-04-20 14:25:27 +0000

    add Graviton4 (no-opt) (customSmallerIsBetter) benchmark result for 56bfbee4699dc73c7d17b524c32c6bf6e0820f22

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1776695116941,
+  "lastUpdate": 1776695126724,
   "repoUrl": "https://github.com/pq-code-package/mldsa-native",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks (opt)": [
@@ -409825,6 +409825,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-DSA-87 verify",
             "value": 373241,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "matthias@zerorisc.com",
+            "name": "Matthias J. Kannwischer",
+            "username": "mkannwischer"
+          },
+          "committer": {
+            "email": "matthias@kannwischer.eu",
+            "name": "Matthias J. Kannwischer",
+            "username": "mkannwischer"
+          },
+          "distinct": true,
+          "id": "56bfbee4699dc73c7d17b524c32c6bf6e0820f22",
+          "message": "CBMC: Remove --no-array-field-sensitivity from all proofs\n\nPorts https://github.com/pq-code-package/mlkem-native/pull/1654\n\nSigned-off-by: Matthias J. Kannwischer <matthias@zerorisc.com>",
+          "timestamp": "2026-04-20T16:20:17+02:00",
+          "tree_id": "234046a24979b138ec35653aa3c20371de754798",
+          "url": "https://github.com/pq-code-package/mldsa-native/commit/56bfbee4699dc73c7d17b524c32c6bf6e0820f22"
+        },
+        "date": 1776695125632,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-DSA-44 keypair",
+            "value": 127982,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 sign",
+            "value": 445654,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 verify",
+            "value": 142076,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 keypair",
+            "value": 219917,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 sign",
+            "value": 721049,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 verify",
+            "value": 223103,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 keypair",
+            "value": 389062,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 sign",
+            "value": 920591,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 verify",
+            "value": 373218,
             "unit": "cycles"
           }
         ]
