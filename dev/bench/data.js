b75d1ad4f3bbb903d6ab0db85f5d9cdf555c8fc8
github-action-benchmark <github@users.noreply.github.com>
2026-04-21 00:27:32 +0000

    add Arm Cortex-A55 (Snapdragon 888) benchmarks (opt) (customSmallerIsBetter) benchmark result for e44679411d3499aa2c77be446c08517eea6e2b2e

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1776731046396,
+  "lastUpdate": 1776731249829,
   "repoUrl": "https://github.com/pq-code-package/mldsa-native",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks (opt)": [
@@ -539190,6 +539190,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-DSA-87 verify",
             "value": 774148,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "rodchap@amazon.com",
+            "name": "Rod Chapman",
+            "username": "rod-chapman"
+          },
+          "committer": {
+            "email": "matthias@kannwischer.eu",
+            "name": "Matthias J. Kannwischer",
+            "username": "mkannwischer"
+          },
+          "distinct": true,
+          "id": "e44679411d3499aa2c77be446c08517eea6e2b2e",
+          "message": "Remove --slice-formula from all Makefiles and set in Makefile.common\n\nAlso, use SMT-only Z3 tactic for proof of poly_invntt_tomont_c() which\nis faster with --slice-formula\n\nSigned-off-by: Rod Chapman <rodchap@amazon.com>",
+          "timestamp": "2026-04-21T01:55:27+02:00",
+          "tree_id": "6a4724a8b6bf65b7fa645f7807113996f3fc92c2",
+          "url": "https://github.com/pq-code-package/mldsa-native/commit/e44679411d3499aa2c77be446c08517eea6e2b2e"
+        },
+        "date": 1776731246595,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-DSA-44 keypair",
+            "value": 269433,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 sign",
+            "value": 806014,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 verify",
+            "value": 272765,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 keypair",
+            "value": 462732,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 sign",
+            "value": 1317634,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 verify",
+            "value": 449250,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 keypair",
+            "value": 796867,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 sign",
+            "value": 1797261,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 verify",
+            "value": 778167,
             "unit": "cycles"
           }
         ]
