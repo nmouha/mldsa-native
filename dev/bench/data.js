9ce7556cffde2557e6c47a1e693894ec0f4914b3
github-action-benchmark <github@users.noreply.github.com>
2026-04-25 13:47:30 +0000

    add Arm Cortex-A72 (Raspberry Pi 4) benchmarks (no-opt) (customSmallerIsBetter) benchmark result for 5a5d3f22c61c39729227745f31b4045506459e05

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1777124715015,
+  "lastUpdate": 1777124847982,
   "repoUrl": "https://github.com/pq-code-package/mldsa-native",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks (opt)": [
@@ -50915,6 +50915,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-DSA-87 verify",
             "value": 873849,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "matthias@zerorisc.com",
+            "name": "Matthias J. Kannwischer",
+            "username": "mkannwischer"
+          },
+          "committer": {
+            "email": "matthias@kannwischer.eu",
+            "name": "Matthias J. Kannwischer",
+            "username": "mkannwischer"
+          },
+          "distinct": true,
+          "id": "5a5d3f22c61c39729227745f31b4045506459e05",
+          "message": "HOL-Light: Remove duplicate SIMD_SIMPLIFY_CONV/TAC from mldsa_utils.ml\n\nAlso drop unused mldsa_utils.ml dep from nttunpack proof.\n\nSigned-off-by: Matthias J. Kannwischer <matthias@zerorisc.com>",
+          "timestamp": "2026-04-25T21:16:14+08:00",
+          "tree_id": "9a0ca23bfb61378eca73e62ba3b2bd2454d33000",
+          "url": "https://github.com/pq-code-package/mldsa-native/commit/5a5d3f22c61c39729227745f31b4045506459e05"
+        },
+        "date": 1777124844749,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-DSA-44 keypair",
+            "value": 301563,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 sign",
+            "value": 1177438,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 verify",
+            "value": 338998,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 keypair",
+            "value": 563049,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 sign",
+            "value": 1916416,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 verify",
+            "value": 542862,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 keypair",
+            "value": 849193,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 sign",
+            "value": 2429837,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 verify",
+            "value": 879667,
             "unit": "cycles"
           }
         ]
