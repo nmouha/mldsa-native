069a8cf982231fa24dc452ea2b69221c166166bd
github-action-benchmark <github@users.noreply.github.com>
2026-04-27 04:50:17 +0000

    add Intel Xeon 4th gen (c7i) (customSmallerIsBetter) benchmark result for e47b1c2fd4308e5dbfa23611d4488b1453f0d50c

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1777265286537,
+  "lastUpdate": 1777265417425,
   "repoUrl": "https://github.com/pq-code-package/mldsa-native",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks (opt)": [
@@ -233475,6 +233475,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-DSA-87 verify",
             "value": 94361,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "matthias@zerorisc.com",
+            "name": "Matthias J. Kannwischer",
+            "username": "mkannwischer"
+          },
+          "committer": {
+            "email": "matthias@kannwischer.eu",
+            "name": "Matthias J. Kannwischer",
+            "username": "mkannwischer"
+          },
+          "distinct": true,
+          "id": "e47b1c2fd4308e5dbfa23611d4488b1453f0d50c",
+          "message": "CI: Re-enable RISE RISC-V runner\n\n- Resolves https://github.com/pq-code-package/mldsa-native/issues/1056\n\nSigned-off-by: Matthias J. Kannwischer <matthias@zerorisc.com>",
+          "timestamp": "2026-04-27T12:46:24+08:00",
+          "tree_id": "5765138e6508d42514a4fc8ba10f0eea8caef42f",
+          "url": "https://github.com/pq-code-package/mldsa-native/commit/e47b1c2fd4308e5dbfa23611d4488b1453f0d50c"
+        },
+        "date": 1777265313499,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-DSA-44 keypair",
+            "value": 34005,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 sign",
+            "value": 119708,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 verify",
+            "value": 38021,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 keypair",
+            "value": 59642,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 sign",
+            "value": 199892,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 verify",
+            "value": 62396,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 keypair",
+            "value": 91826,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 sign",
+            "value": 231158,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 verify",
+            "value": 93777,
             "unit": "cycles"
           }
         ]
