71516fef3d853d34a1929ab47949913d562108b5
github-action-benchmark <github@users.noreply.github.com>
2026-04-27 04:52:28 +0000

    add Graviton3 (no-opt) (customSmallerIsBetter) benchmark result for e47b1c2fd4308e5dbfa23611d4488b1453f0d50c

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1777265540741,
+  "lastUpdate": 1777265547935,
   "repoUrl": "https://github.com/pq-code-package/mldsa-native",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks (opt)": [
@@ -471300,6 +471300,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-DSA-87 verify",
             "value": 402505,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "matthias@zerorisc.com",
+            "name": "Matthias J. Kannwischer",
+            "username": "mkannwischer"
+          },
+          "committer": {
+            "email": "matthias@kannwischer.eu",
+            "name": "Matthias J. Kannwischer",
+            "username": "mkannwischer"
+          },
+          "distinct": true,
+          "id": "e47b1c2fd4308e5dbfa23611d4488b1453f0d50c",
+          "message": "CI: Re-enable RISE RISC-V runner\n\n- Resolves https://github.com/pq-code-package/mldsa-native/issues/1056\n\nSigned-off-by: Matthias J. Kannwischer <matthias@zerorisc.com>",
+          "timestamp": "2026-04-27T12:46:24+08:00",
+          "tree_id": "5765138e6508d42514a4fc8ba10f0eea8caef42f",
+          "url": "https://github.com/pq-code-package/mldsa-native/commit/e47b1c2fd4308e5dbfa23611d4488b1453f0d50c"
+        },
+        "date": 1777265547145,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-DSA-44 keypair",
+            "value": 138178,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 sign",
+            "value": 482432,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 verify",
+            "value": 156415,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 keypair",
+            "value": 241311,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 sign",
+            "value": 786572,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 verify",
+            "value": 241130,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 keypair",
+            "value": 443649,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 sign",
+            "value": 1006282,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 verify",
+            "value": 402544,
             "unit": "cycles"
           }
         ]
