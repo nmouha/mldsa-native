658f87f475833c842a94dceb6cd4722b728d4a41
github-action-benchmark <github@users.noreply.github.com>
2026-04-30 14:32:02 +0000

    add Graviton2 (no-opt) (customSmallerIsBetter) benchmark result for 34ce534111eb9b889d33b5884052c6f0949bb5be

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1777559511993,
+  "lastUpdate": 1777559521443,
   "repoUrl": "https://github.com/pq-code-package/mldsa-native",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks (opt)": [
@@ -535811,6 +535811,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-DSA-87 verify",
             "value": 617427,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "matthias@zerorisc.com",
+            "name": "Matthias J. Kannwischer",
+            "username": "mkannwischer"
+          },
+          "committer": {
+            "email": "matthias@kannwischer.eu",
+            "name": "Matthias J. Kannwischer",
+            "username": "mkannwischer"
+          },
+          "distinct": true,
+          "id": "34ce534111eb9b889d33b5884052c6f0949bb5be",
+          "message": "x86_64: Fix wrong qbound 87*q -> 31*q in use_hint_32\n\nuse_hint_32 in the AVX2 backend contains a wrong constant\nthat was copied from use_hint_88.\nThis disabled the check if w1=16 after the decompose step.\nLuckily, this cancels out later on in the use_hint for\nnon-trivial reasons. We have also exhaustively tested that\nthis code produces correct results for all inputs.\n\nWe will be able to optimize this away in a follow-up commit.\nFor now, this commit just corrects the constant to avoid confusion.\n\n- Fixes https://github.com/pq-code-package/mldsa-native/issues/1074\n\nSigned-off-by: Matthias J. Kannwischer <matthias@zerorisc.com>",
+          "timestamp": "2026-04-30T22:25:24+08:00",
+          "tree_id": "443472cddf69a74b63818e25b152a87a721c3957",
+          "url": "https://github.com/pq-code-package/mldsa-native/commit/34ce534111eb9b889d33b5884052c6f0949bb5be"
+        },
+        "date": 1777559518972,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-DSA-44 keypair",
+            "value": 212544,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 sign",
+            "value": 760719,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 verify",
+            "value": 233229,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 keypair",
+            "value": 379339,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 sign",
+            "value": 1247871,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 verify",
+            "value": 372235,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 keypair",
+            "value": 621525,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 sign",
+            "value": 1584566,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 verify",
+            "value": 617536,
             "unit": "cycles"
           }
         ]
