284ad317490b11a03991c93bc9a212adc4afcf04
github-action-benchmark <github@users.noreply.github.com>
2026-05-01 14:20:01 +0000

    add Graviton3 (customSmallerIsBetter) benchmark result for 5c07dcbd102a76b74af5866fbf1f3836d48049a8

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1777645182126,
+  "lastUpdate": 1777645200953,
   "repoUrl": "https://github.com/pq-code-package/mldsa-native",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks (opt)": [
@@ -455708,6 +455708,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-DSA-87 verify",
             "value": 205318,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "matthias@kannwischer.eu",
+            "name": "Matthias J. Kannwischer",
+            "username": "mkannwischer"
+          },
+          "committer": {
+            "email": "matthias@kannwischer.eu",
+            "name": "Matthias J. Kannwischer",
+            "username": "mkannwischer"
+          },
+          "distinct": true,
+          "id": "5c07dcbd102a76b74af5866fbf1f3836d48049a8",
+          "message": "CBMC: Add CBMC proof for aarch64 polyz_unpack\n\nAdd CBMC contracts for native AArch64 polyz_unpack_17 and polyz_unpack_19\nfollowing corresponding HOL-Light specs.\n\nSigned-off-by: Matthias J. Kannwischer <matthias@kannwischer.eu>",
+          "timestamp": "2026-05-01T22:13:51+08:00",
+          "tree_id": "0b875dfb34d5746e0c45faffcf8ace5403a8d74f",
+          "url": "https://github.com/pq-code-package/mldsa-native/commit/5c07dcbd102a76b74af5866fbf1f3836d48049a8"
+        },
+        "date": 1777645032211,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-DSA-44 keypair",
+            "value": 71656,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 sign",
+            "value": 213465,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 verify",
+            "value": 75723,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 keypair",
+            "value": 126768,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 sign",
+            "value": 349470,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 verify",
+            "value": 125259,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 keypair",
+            "value": 207524,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 sign",
+            "value": 449669,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 verify",
+            "value": 205343,
             "unit": "cycles"
           }
         ]
