4630dbf95f5486fc57273eb323ff5d9e6a393c65
github-action-benchmark <github@users.noreply.github.com>
2026-05-02 18:36:05 +0000

    add Arm Cortex-A55 (Snapdragon 888) benchmarks (opt) (customSmallerIsBetter) benchmark result for 377974e4259b28235a5291e3611a0465f652ebdb

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1777746537359,
+  "lastUpdate": 1777746962734,
   "repoUrl": "https://github.com/pq-code-package/mldsa-native",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks (opt)": [
@@ -571068,6 +571068,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-DSA-87 verify",
             "value": 776665,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "matthias@zerorisc.com",
+            "name": "Matthias J. Kannwischer",
+            "username": "mkannwischer"
+          },
+          "committer": {
+            "email": "beckphan@amazon.co.uk",
+            "name": "Hanno Becker",
+            "username": "hanno-becker"
+          },
+          "distinct": true,
+          "id": "377974e4259b28235a5291e3611a0465f652ebdb",
+          "message": "Lint: Ignore NIST publications in markdown-link-check\n\nhttp://nvlpubs.nist.gov/nistpubs/FIPS/NIST.FIPS.202.pdf has repeatedly caused\nerrors in CI due to not being available.\nThis commit adds it to the ignore list.\n\nWhile at it, this also copies the\n\"^https://github\\\\.com/pq-code-package/mldsa-native/compare/.*\\\\.\\\\.\\\\.v\"\nexception from mlkem-native which allows us to reference a release diff before\nthe corresponding tag has been created.\n\nSigned-off-by: Matthias J. Kannwischer <matthias@zerorisc.com>",
+          "timestamp": "2026-05-02T19:21:16+01:00",
+          "tree_id": "8217abf6f96804ff64e320e5891a39a800e98b7b",
+          "url": "https://github.com/pq-code-package/mldsa-native/commit/377974e4259b28235a5291e3611a0465f652ebdb"
+        },
+        "date": 1777746271284,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-DSA-44 keypair",
+            "value": 270031,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 sign",
+            "value": 812990,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 verify",
+            "value": 273570,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 keypair",
+            "value": 464017,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 sign",
+            "value": 1323917,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 verify",
+            "value": 452183,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 keypair",
+            "value": 802866,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 sign",
+            "value": 1826468,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 verify",
+            "value": 789026,
             "unit": "cycles"
           }
         ]
