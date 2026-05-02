6c6a9824fd302378d4405dbe0f01544a9e4ccb28
github-action-benchmark <github@users.noreply.github.com>
2026-05-02 23:26:00 +0000

    add Intel Xeon 4th gen (c7i) (customSmallerIsBetter) benchmark result for 339782e3c1d9730374a849cd157097833b4dc6b7

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1777764216024,
+  "lastUpdate": 1777764360240,
   "repoUrl": "https://github.com/pq-code-package/mldsa-native",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks (opt)": [
@@ -243480,6 +243480,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-DSA-87 verify",
             "value": 93384,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "matthias@kannwischer.eu",
+            "name": "Matthias J. Kannwischer",
+            "username": "mkannwischer"
+          },
+          "committer": {
+            "email": "matthias@kannwischer.eu",
+            "name": "Matthias J. Kannwischer",
+            "username": "mkannwischer"
+          },
+          "distinct": true,
+          "id": "339782e3c1d9730374a849cd157097833b4dc6b7",
+          "message": "HOL-Light/x86_64: Replace Keccakx4 intrinsics with AVX2 assembly and prove correct\n\n- Ports https://github.com/pq-code-package/mlkem-native/pull/1576\n\nSigned-off-by: Matthias J. Kannwischer <matthias@kannwischer.eu>",
+          "timestamp": "2026-05-03T07:21:57+08:00",
+          "tree_id": "2f1cbf587df962728b9da905cfc76c9696586e46",
+          "url": "https://github.com/pq-code-package/mldsa-native/commit/339782e3c1d9730374a849cd157097833b4dc6b7"
+        },
+        "date": 1777764241123,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-DSA-44 keypair",
+            "value": 45514,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 sign",
+            "value": 136652,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 verify",
+            "value": 51971,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 keypair",
+            "value": 79830,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 sign",
+            "value": 224543,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 verify",
+            "value": 79258,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 keypair",
+            "value": 151720,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 sign",
+            "value": 284954,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 verify",
+            "value": 128257,
             "unit": "cycles"
           }
         ]
