05de13c94710d4564a2bb08a5f6d04c7cd416ab8
github-action-benchmark <github@users.noreply.github.com>
2026-05-05 04:32:54 +0000

    add Arm Cortex-A72 (Raspberry Pi 4) benchmarks (no-opt) (customSmallerIsBetter) benchmark result for c1725f36e31aa247b0cec09ed8699cfb9c8575f3

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1777955440542,
+  "lastUpdate": 1777955571330,
   "repoUrl": "https://github.com/pq-code-package/mldsa-native",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks (opt)": [
@@ -54020,6 +54020,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-DSA-87 verify",
             "value": 879669,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "matthias@kannwischer.eu",
+            "name": "Matthias J. Kannwischer",
+            "username": "mkannwischer"
+          },
+          "committer": {
+            "email": "matthias@kannwischer.eu",
+            "name": "Matthias J. Kannwischer",
+            "username": "mkannwischer"
+          },
+          "distinct": true,
+          "id": "c1725f36e31aa247b0cec09ed8699cfb9c8575f3",
+          "message": "CBMC/AArch64: Add contracts and proof poly_decompose asm functions\n\nSigned-off-by: Matthias J. Kannwischer <matthias@kannwischer.eu>",
+          "timestamp": "2026-05-05T12:15:09+08:00",
+          "tree_id": "a4d1c9b559d0ec5a8b079a67edf2d3520f5e107b",
+          "url": "https://github.com/pq-code-package/mldsa-native/commit/c1725f36e31aa247b0cec09ed8699cfb9c8575f3"
+        },
+        "date": 1777955567852,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-DSA-44 keypair",
+            "value": 301758,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 sign",
+            "value": 1145744,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 verify",
+            "value": 326196,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 keypair",
+            "value": 555999,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 sign",
+            "value": 1911614,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 verify",
+            "value": 528099,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 keypair",
+            "value": 841771,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 sign",
+            "value": 2391806,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 verify",
+            "value": 870290,
             "unit": "cycles"
           }
         ]
