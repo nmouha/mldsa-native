26f7700eef2d14d65f98db452ccf04a5c632cb7f
github-action-benchmark <github@users.noreply.github.com>
2026-05-10 03:53:08 +0000

    add Graviton2 (customSmallerIsBetter) benchmark result for 5baebc6f2c11d8e9073b6e4aa6bf934e818b0f3a

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1778385169394,
+  "lastUpdate": 1778385187655,
   "repoUrl": "https://github.com/pq-code-package/mldsa-native",
   "entries": {
     "Arm Cortex-A72 (Raspberry Pi 4) benchmarks (opt)": [
@@ -539946,6 +539946,75 @@ window.BENCHMARK_DATA = {
           {
             "name": "ML-DSA-87 verify",
             "value": 319316,
+            "unit": "cycles"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "matthias@zerorisc.com",
+            "name": "Matthias J. Kannwischer",
+            "username": "mkannwischer"
+          },
+          "committer": {
+            "email": "matthias@kannwischer.eu",
+            "name": "Matthias J. Kannwischer",
+            "username": "mkannwischer"
+          },
+          "distinct": true,
+          "id": "5baebc6f2c11d8e9073b6e4aa6bf934e818b0f3a",
+          "message": "README: Note other side-channel and fault attacks are currently out of scope\n\nPorts https://github.com/pq-code-package/mlkem-native/pull/1685\n\nSigned-off-by: Matthias J. Kannwischer <matthias@zerorisc.com>",
+          "timestamp": "2026-05-10T11:45:32+08:00",
+          "tree_id": "064cfdcf3e0ad4589ca22063bcc3b5a24fcbc5e1",
+          "url": "https://github.com/pq-code-package/mldsa-native/commit/5baebc6f2c11d8e9073b6e4aa6bf934e818b0f3a"
+        },
+        "date": 1778384914185,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "ML-DSA-44 keypair",
+            "value": 112740,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 sign",
+            "value": 356988,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-44 verify",
+            "value": 117433,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 keypair",
+            "value": 195092,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 sign",
+            "value": 588035,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-65 verify",
+            "value": 193730,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 keypair",
+            "value": 322204,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 sign",
+            "value": 754802,
+            "unit": "cycles"
+          },
+          {
+            "name": "ML-DSA-87 verify",
+            "value": 319347,
             "unit": "cycles"
           }
         ]
